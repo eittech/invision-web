fd313138fbaf49add167e6bfbf2398dc1d70fcc3
Jesus Laya <jesusgerard2008@gmail.com>
2019-07-22 17:00:48 -0400

    Implemantacion Idioma Ingles

---
+++ lang/en/home.php
@@ -0,0 +1,29 @@
+<?php
+
+// Textos de datos
+
+// Home
+$lang['home']['title'] = "Invision";
+$lang['home']['header_title_one'] = "INVEST IN INVISION<br> WITH A GUARANTEE<br>  OF SUCCESS";
+$lang['home']['header_title_two'] = "Transparency and high profitability in the short term<br> NVISION has the right investment for you";
+$lang['home']['body_section_h2'] = "INVEST IN INVISION WITH A GUARANTEE OF SUCCESS";
+$lang['home']['column_one_second_p_one'] = "We are a solid real estate investment company
+                                            specialized in planning and executing projects
+                                            in the United States. We offer our investors
+                                            the perfect combination between high
+                                            profitability and rates of return on market,
+                                            with guarantees of invested capital protection.";
+$lang['home']['column_one_second_p_two'] = "Learn more about NVISION REAL ESTATE INVESTMENTS.";
+$lang['home']['section_wrapper'] = "O QUE NOSSOS CLIENTES FALAM";
+$lang['home']['carousel_caption_desc_one'] = "Fiquei muito satisfeito com o resultado e renovei o investimento com a empresa em outro projeto de construção.";
+$lang['home']['carousel_caption_desc_two'] = "Até agora estou muito satisfeito dos resultados da empresa, recomendo para qualquer fazer negócio com eles.";
+$lang['home']['carousel_caption_desc_three'] = "Definitivamente vou fazer parte do desenvolvimento futuro da Invision.";
+$lang['home']['footer_title_one'] = "Últimos Investimentos";
+$lang['home']['footer_title_two'] = "Aguardamos o seu Contato";
+$lang['home']['footer_contact_icons'] = "Encontre-nos aqui";
+
+
+
+
+
+?>
