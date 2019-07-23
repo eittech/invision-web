6b636a19fc0794529c3b623e252a6682701ba26e
Jesus Laya <jesusgerard2008@gmail.com>
2019-07-23 16:11:48 -0400

    Validacion segun el idioma seleccionado para que no se muestre el idioma actual

--- menu.php
+++ menu.php
@@ -1,5 +1,7 @@
 <?php
 
+    $idiom_active = $_GET['lang'];
+
     $segment = $_SERVER['PHP_SELF'];
     $segment = explode("/", $segment);
     $segment = $segment[2];
@@ -42,7 +44,41 @@
             <li class="<?php echo $current_page_item_6; ?>">
                 <a href="contact.php<?php echo $idiom; ?>"><span><?php echo $lang['menu']['contact']; ?></span></a>
             </li>
-        </ul>
+            <!-- Menu de idioma -->
+                <li style="display:block;" class="nav-item dropdown hidden-xs hidden-sm submenu last" id="li_language">
+                    <a class="nav-link smooth-scroll dropdown-toggle count-info page-scroll" data-toggle="dropdown" href="#" title="Idioma">
+                        <!--<img src="images/language.png">-->
+                        <span class="label label-warning" id="span_num_respuestas">Language</span>
+                    </a>
+                    <ul class="dropdown-menu" style="background-color: rgb(93, 104, 174) !important; color: rgb(255, 255, 255) !important; display: none;">
+                        <li <?php if($idiom_active == "pt"){ echo "hidden"; } ?>>
+                            <a class="nav-link smooth-scroll" href="index.php?lang=pt" style="color:#f2f2f2 !important">
+                                <div>
+                                    <b>Portugues</b>
+                                </div>
+                            </a>
+                        </li>
+                        <li <?php if($idiom_active == "en"){ echo "hidden"; } ?>>
+                            <a class="nav-link smooth-scroll" href="index.php?lang=en" style="color:#f2f2f2 !important">
+                                <div>
+                                    <b>English</b>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li <?php if($idiom_active == "es"){ echo "hidden"; } ?>>
+                            <a class="nav-link smooth-scroll" href="index.php?lang=es" style="color:#f2f2f2 !important">
+                                <div>
+                                    <b>Spanish</b>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="divider last-item"></li>
+                    </ul>
+                    <span class="menu-toggle"></span>
+                </li>
+            </ul>
+        
     </nav>
     <a class="responsive-menu-toggle" href="#"><i class="icon-menu"></i></a>
 </div>
